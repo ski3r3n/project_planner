2b0723f071781666bb3126709d2686c458951bf1
Kie Ren <kieren.siew@gmail.com>
2025-02-13 16:39:29 +0800

    i fixed the mobile friendlyness. also readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
       display={"flex"}
       flexDirection={{ base: "column", lg: "row" }}
       alignItems={{ base: "center", lg: "flex-start" }}>
-      {/* Left Side - Text Content */}
       <Box
+        zIndex={"1"}
         flex={1}
         display={"flex"}
         flexDirection={"column"}
@@ -40,34 +40,45 @@ export default function Home() {
         </Heading>
 
         <Box fontSize={"xl"} display={"flex"} mt={{ lg: "28", base: "12" }}>
-          <Box mr={"12"} padding={"1.5"}>EXPERIENCE EFFICIENCY</Box>
+          <Box mr={"12"} padding={"1.5"}>
+            EXPERIENCE EFFICIENCY
+          </Box>
           <Button padding={"3"} rounded={"xl"} backgroundColor={"white"}>
             SIGN UP NOW
           </Button>
         </Box>
       </Box>
 
-      {/* Right Side - Image */}
       <Box
-        flex={1}
-        mt={"-12"}
-        mr={"-12"}
-        display={"flex"}
-        justifyContent={{ base: "center", lg: "flex-end" }}>
-        <Image
-          src={"/womanWritingOnCalendar.jpg"}
-          h={"100vh"}
-          width={"2xl"}
-        />
+        display="block"
+        position="absolute"
+        top="0"
+        height="100vh"
+        width="100%"
+        bgImage= "url('womanWritingOnCalendar.jpg')"
+        z-index="-3"
+        lg={{
+          position: "static",
+          flex: 1,
+          mt: "-12",
+          mr: "-12",
+          ml: "20",
+          width: "xl",
+          h: "100vh",
+          bgImage: "url('womanWritingOnCalendar.jpg')",
+          display: "flex",
+          justifyContent: "flex-end",
+        }}>
         <Box
-          position={"absolute"}
-          w={"2xl"}
-          h={"100%"}
-          bgGradient={"to-r"}
-          gradientFrom={"ourBg"}
-          gradientTo={"transparent"}
-          >
-        </Box>
+          bgImage={{
+            lg: "linear-gradient(to right, #e4e6e9, transparent)",
+            base: "linear-gradient(to top, #e4e6e9, rgba(228, 230, 233, 0.36))",
+          }}
+          h={{ lg: "100vh", base: "80vh" }}
+          width={"100%"}
+        />
+        <Box h={{ lg: "0vh", base: "20vh" }} bgColor={"#e4e6e9"}
+          width={"100%"}></Box>
       </Box>
     </Box>
   );
