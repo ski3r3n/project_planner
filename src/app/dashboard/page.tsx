b968d976644305022ea7f4f8cf6c08a95d55fdb5
Kie Ren <kieren.siew@gmail.com>
2025-06-19 15:08:45 +0800

    format taskcard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,32 @@
 import Sidebar from "@/components/sidebar";
 import TaskCard from "@/components/taskcard";
 import { Box } from "@chakra-ui/react";
-
+const tasks = [
+  {
+    type: "Meeting",
+    name: "Discuss stuff",
+    project: "Project Planner",
+    time: "3/5/2025 13:05",
+    link: "https://meet.google.com",
+    taskId: 1,
+  },
+  {
+    type: "Task",
+    name: "Finish UI",
+    project: "Project Planner",
+    time: "10/6/2025",
+    link: "https://github.com/ski3r3n/project_planner",
+    taskId: 2,
+  },
+  {
+    type: "Goal",
+    name: "Finish everything",
+    project: "Project Planner",
+    time: "5/9/2025",
+    link: "https://github.com/ski3r3n/project_planner",
+    taskId: 3,
+  },
+];
 export default function Dashboard() {
   return (
     <>
@@ -17,32 +42,9 @@ export default function Dashboard() {
             {" "}
             {/* load tasks and meetings of user */}
             
-              <TaskCard
-                type="Meeting"
-                name="Discuss stuff"
-                project="Project Planner"
-                time="3/5/2025 13:05"
-                link="meet.google.com"
-                taskId={1}
-              />
-            
-              <TaskCard
-                type="Task"
-                name="Finish UI"
-                project="Project Planner"
-                time="10/6/2025"
-                link="github.com/ski3r3n/project_planner"
-                taskId={2}
-              />
-            
-              <TaskCard
-                type="Goal"
-                name="Finish everything"
-                project="Project Planner"
-                time="5/9/2025"
-                link="github.com/ski3r3n/project_planner"
-                taskId={3}
-              />
+            {tasks.map((task) => (
+            <TaskCard key={task.taskId} {...task} />
+          ))}
           </Box>
         </Sidebar>
       </Box>
